3b2f9a15cb48e24e10db620f008d3e488d9711e8
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2025-07-27 11:54:49 -0300

    feat: crate generics Identity and Entity

---
+++ property-service/src/core/generics/Entity.ts
@@ -0,0 +1,15 @@
+import Identity from "./Identity";
+
+export default class Entity<T> {
+    private valueIdentity: Identity;
+    protected props: T;
+
+    protected constructor(props: T, id?: Identity) {
+        this.props = props;
+        this.valueIdentity = id ?? new Identity();
+    }
+
+    get identityId(): Identity {
+        return this.valueIdentity;
+    }
+}
---
+++ property-service/src/core/generics/Identity.ts
@@ -0,0 +1,13 @@
+import { randomUUID } from "crypto";
+
+export default class Identity {
+    private _id: string;
+
+    constructor(value?: string) {
+        this._id = value ?? randomUUID();
+    }
+
+    get valueId(): string {
+        return this._id;
+    }
+}
