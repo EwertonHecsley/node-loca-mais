1a32686b88cf5669e1e45566790f14cc38fbf8e4
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2025-08-16 12:24:57 -0300

    feat: Create instance service Prisma ORM

---
+++ src/infrastructure/database/prisma/singletonPrisma.ts
@@ -0,0 +1,10 @@
+import { PrismaService } from "./PrismaService";
+
+let prisma: PrismaService;
+
+export function getPrismaInstance(): PrismaService {
+    if (!prisma) {
+        prisma = new PrismaService();
+    }
+    return prisma;
+}
